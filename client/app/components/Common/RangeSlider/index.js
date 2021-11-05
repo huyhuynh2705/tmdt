edcab3d67802149d9d6f77c899785d7f69854255
Huynh Quang Huy <huy.huynh270520@gmail.com>
2021-11-05 19:18:24 +0700

    first commit

---
+++ client/app/components/Common/RangeSlider/index.js
@@ -0,0 +1,94 @@
+/**
+ *
+ *  Range Slider
+ *
+ */
+
+import React from 'react';
+import Slider, { SliderTooltip } from 'rc-slider';
+
+const { createSliderWithTooltip } = Slider;
+const Range = createSliderWithTooltip(Slider.Range);
+const { Handle } = Slider;
+
+const handle = (props) => {
+	const { value, dragging, index, ...restProps } = props;
+	return (
+		<SliderTooltip prefixCls='rc-slider-tooltip' overlay={`$${value}`} visible={dragging} placement='top' key={index}>
+			<Handle value={value} {...restProps} />
+		</SliderTooltip>
+	);
+};
+
+class RangeSlider extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			sliderValue: 50,
+			rangeValue: [100, 500],
+		};
+	}
+
+	onSliderChange = (v) => {
+		this.setState({
+			sliderValue: v,
+		});
+	};
+
+	onRangeChange = (v) => {
+		this.setState({
+			rangeValue: v,
+		});
+	};
+
+	onAfterSliderChange = (value) => {
+		this.props.onChange(value);
+	};
+
+	onAfterRangeChange = (value) => {
+		this.props.onChange(value);
+	};
+
+	render() {
+		const { type, marks, step, defaultValue } = this.props;
+		const { sliderValue, rangeValue } = this.state;
+
+		return (
+			<>
+				{type === 'slider' ? (
+					<Slider
+						className='slider'
+						dots
+						reverse
+						step={step}
+						defaultValue={defaultValue}
+						marks={marks}
+						value={sliderValue}
+						onChange={this.onSliderChange}
+						onAfterChange={this.onAfterSliderChange}
+					/>
+				) : (
+					<Range
+						className='slider'
+						pushable={100}
+						min={100}
+						max={500}
+						marks={marks}
+						handle={handle}
+						tipFormatter={(value) => `${value}k`}
+						defaultValue={defaultValue}
+						value={rangeValue}
+						onChange={this.onRangeChange}
+						onAfterChange={this.onAfterRangeChange}
+					/>
+				)}
+			</>
+		);
+	}
+}
+
+RangeSlider.defaultProps = {
+	type: 'range',
+};
+
+export default RangeSlider;
